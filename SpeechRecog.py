3e2cdf24b34d19a2b50b110b0b384a68bb53f2fc
vaqueni <84617010+vaqueni@users.noreply.github.com>
2022-10-04 16:34:19 +0900

    Add files via upload

---
+++ SpeechRecog.py
@@ -0,0 +1,24 @@
+#SpeechRecog.python
+#2022-10-02 ver
+#https://prlabhotelshoe.tistory.com/8?category=995004
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+# audio_file = sr.AudioFile('./test_file.wav')
+audio_file = sr.AudioFile('./news.wav')
+
+with audio_file as source:
+    audio = r.record(source)
+
+
+#sys.stdout = open('stdout.txt','w')
+f = open("output.txt",'w')
+# f.close()
+
+Txt = r.recognize_google(audio,language = 'ko-KR')
+
+
+print(Txt)
+f.write(Txt)
+#sys.stdout.close()
